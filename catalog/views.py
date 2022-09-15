38a1bcb847f43d3c805af45ef8f5a53c1946e0f8
Maxon <notmax2000@mail.ru>
2022-09-15 21:37:51 +0700

    123

--- catalog/views.py
+++ catalog/views.py
@@ -15,12 +15,17 @@ def index(request):
     num_genres = Genre.objects.count()
     num_books_having_Harry = Book.objects.filter(title__contains='Гарри').count()
 
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
     # Отрисовка HTML-шаблона index.html с данными внутри
     # переменной контекста context
     return render(
         request,
         'index.html',
-        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors,'num_genres':num_genres,'num_books_having_Harry':num_books_having_Harry},
+        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors,
+                 'num_genres':num_genres,'num_books_having_Harry':num_books_having_Harry,'num_visits':num_visits},
     )
 
 class BookListView(generic.ListView):
@@ -34,5 +39,10 @@ class BookDetailView(generic.DetailView):
 class AuthorListView(generic.ListView):
     model = Author
     paginate_by = 10
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+    paginate_by = 10
+
 
 
