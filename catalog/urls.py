d4f0cdc0cb10c11fdc83d763caacb5d8588c78e4
2two2d <notmax2000@mail.ru>
2022-09-17 11:31:01 +0700

    9

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
 from django.urls import re_path as url
-from django.urls import path
 
 
 urlpatterns = [
@@ -12,4 +11,21 @@ urlpatterns = [
     url(r'^authors/(?P<pk>\d+)$', views.AuthorDetailView.as_view(), name='author-detail'),
     url(r'^mybooks/$', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
     url(r'^borrowed-books/$', views.BorrowedBooksLibrariansOnlyView.as_view(), name='borrowed-books')
+]
+
+urlpatterns += [
+    url(r'^book/(?P<pk>[-\w]+)/renew/$', views.renew_book_librarian, name='renew-book-librarian'),
+]
+
+
+urlpatterns += [
+    url(r'^author/create/$', views.AuthorCreate.as_view(), name='author_create'),
+    url(r'^author/(?P<pk>\d+)/update/$', views.AuthorUpdate.as_view(), name='author_update'),
+    url(r'^author/(?P<pk>\d+)/delete/$', views.AuthorDelete.as_view(), name='author_delete'),
+]
+
+urlpatterns += [
+    url(r'^book/create/$', views.AuthorCreate.as_view(), name='book_create'),
+    url(r'^book/(?P<pk>\d+)/update/$', views.AuthorUpdate.as_view(), name='book_update'),
+    url(r'^book/(?P<pk>\d+)/delete/$', views.AuthorDelete.as_view(), name='book_delete'),
 ]
